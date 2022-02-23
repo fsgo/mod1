315d05830359d07be69e0abff20dcd839c3f4bb8
hidu <duv123+github@gmail.com>
2022-02-23 11:33:19 +0800

    fmt

--- version.go
+++ version.go
@@ -1,8 +1,6 @@
-/*
- * Copyright(C) 2020 github.com/hidu  All Rights Reserved.
- * Author: hidu (duv123+git@baidu.com)
- * Date: 2020/7/3
- */
+// Copyright(C) 2020 github.com/hidu  All Rights Reserved.
+// Author: hidu (duv123+git@baidu.com)
+// Date: 2020/7/3
 
 package mod1
 
@@ -10,7 +8,6 @@ import (
 	"runtime"
 )
 
-
-func Version()string{
-	return "mod1{ mod_version:1.0.2 go:"+runtime.Version()+" }"
+func Version() string {
+	return "mod1{ mod_version:1.0.2 go:" + runtime.Version() + " }"
 }
